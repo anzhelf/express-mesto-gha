c9866823bb418def46156d7c705efa77e6bc468f
Anzhelika Shilyaeva <ana.anajel@yandex.ru>
2023-04-03 17:22:51 +0300

    Поправить значение ошибок

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
   let payload;
@@ -7,7 +8,7 @@ module.exports = (req, res, next) => {
     const token = req.cookies.jwt;
     payload = jwt.verify(token, 'some-secret-key');
   } catch (e) {
-    next(e);
+    next(new UnauthorizedError('Необходима авторизация.'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
